0d0b6256bd71d0189a8d9391c7379f4b4a1a9e38
Sheikh Nazmul islam <152971502+NazmulNiR1@users.noreply.github.com>
2024-02-21 09:35:18 +0600

    variable

---
+++ variable_pyton.py
@@ -0,0 +1,13 @@
+a = 10
+print('a')#is there is variable then dont neet to "" 
+#like this print(10) or print(S)
+#if you want to print a string then you have to use ""
+a= 30
+b= 40 
+c= 50
+print(a,b,c)
+print(a+b)
+print(a-b)
+
+    a,b,c,= 10,20,30
+print(a,b,c)
